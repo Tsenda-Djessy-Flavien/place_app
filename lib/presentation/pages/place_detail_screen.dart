aacc41d00c5be54a57a7310865ae2d84b98f7c32
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-07-12 13:28:40 +0100

    (PlaceListWidget & PlaceDetailScreen)CI:Sortie des données de localisation

--- lib/presentation/pages/place_detail_screen.dart
+++ lib/presentation/pages/place_detail_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:places_app/domain/models/place_model.dart';
+import 'package:places_app/network/api/google_maps_key.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key, required this.places});
 
   final PlaceModel places;
+
+  String get locationImage {
+    final lat = places.location.latitude;
+    final lng = places.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=$apiKey';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +25,43 @@ class PlaceDetailScreen extends StatelessWidget {
             width: double.infinity,
             height: double.infinity,
           ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    places.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
